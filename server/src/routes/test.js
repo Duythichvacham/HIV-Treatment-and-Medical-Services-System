be9f4ee310fd039bf722c8eb73e67273b156cc5e
Duythichvacham <proxmen009@gmail.com>
2025-06-21 13:13:51 +0700

    fix forget import

--- server/src/routes/test.js
+++ server/src/routes/test.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const authenticateToken = require('../middleware/authMiddleware');
+const authenticateToken = require("../middleware/authMiddleware");
 
-const testController = require('../controllers/testController');
-
-// PATCH /api/v1/test-requests/:id/status
-router.patch('/:id/status',authenticateToken, testController.updateTestRequestExamStatus);
-
-// GET /api/v1/lab/test-notes/:test_note_id
-router.get('/:test_note_id',authenticateToken, testController.getTestNoteDetail);
-
-// POST /api/v1/lab/test-results (nhập kết quả xét nghiệm và hoàn thành); 
-router.post('/',authenticateToken, testController.createTestResultAndComplete);
-module.exports = router;
-=======
 const testController = require("../controllers/testController");
 
 // PATCH /api/v1/test-requests/:id/status
-router.patch("/:id/status", testController.updateTestRequestExamStatus);
+router.patch(
+  "/:id/status",
+  authenticateToken,
+  testController.updateTestRequestExamStatus
+);
 
 // GET /api/v1/lab/test-notes/:test_note_id
-router.get("/:test_note_id", testController.getTestNoteDetail);
+router.get(
+  "/:test_note_id",
+  authenticateToken,
+  testController.getTestNoteDetail
+);
 
 // POST /api/v1/lab/test-results (nhập kết quả xét nghiệm và hoàn thành);
-router.post("/", testController.createTestResultAndComplete);
+router.post("/", authenticateToken, testController.createTestResultAndComplete);
 module.exports = router;
->>>>>>> 9fbbd52f42e3efc544f7b5f619c0a3752d607122
