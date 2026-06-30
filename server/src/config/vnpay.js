4c9458cd5334e4acf7ee0befb157684690b35f00
Duythichvacham <proxmen009@gmai.com>
2026-06-30 20:06:29 +0700

    vnpay

--- server/src/config/vnpay.js
+++ server/src/config/vnpay.js
@@ -1,6 +1,7 @@
+
 module.exports = {
-  vnp_TmnCode: VNPAY_TMN_CODE,
-  vnp_HashSecret: VNPAY_HASH_SECRET,
+  vnp_TmnCode: process.env.VNPAY_TMN_CODE,
+  vnp_HashSecret: process.env.VNPAY_HASH_SECRET,
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
   vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
   // vnp_ReturnUrl: "http://localhost:5000/api/payment/vnpay_return",
